5412f59d8370537b297f0c769b882508b5c950e5
codiy <mail@codiy.net>
2026-02-24 17:48:04 -0500

    Change Path

--- 28/solution.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-
-haystack = "leetcode"
-needle = "leeto"
-print(Solution().strStr(haystack, needle))
